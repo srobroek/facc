6fdf289f13ae16e8151f575ebd24056ace1b164b
Sjors Robroek <s.robroek@vxsan.com>
2022-04-21 09:04:40 +0400

    bumped version to test releaser

--- cmd/facc/root.go
+++ cmd/facc/root.go
@@ -11,14 +11,12 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "facc",
-	Short: "This cli template shows the date and time in the terminal",
-	Long: `This is a template CLI application, which can be used as a boilerplate for awesome CLI tools written in Go.
-This template prints the date or time to the terminal.`,
-	Example: `cli-template date
-cli-template date --format 20060102
-cli-template time
-cli-template time --live`,
-	Version: "v0.0.1", // <---VERSION---> Updating this version, will also create a new GitHub release.
+	Short: "Facc your firewall",
+	Long:  ``,
+	Example: `facc 
+facc
+`,
+	Version: "v0.0.2", // <---VERSION---> Updating this version, will also create a new GitHub release.
 	// Uncomment the following lines if your bare application has an action associated with it:
 	// RunE: func(cmd *cobra.Command, args []string) error {
 	// 	// Your code here
